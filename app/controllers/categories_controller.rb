099cf4e3cf6d0622d8d513c6f8acfe47c0846fd5
yufuworks <yufuworks@outlook.jp>
2019-05-06 20:28:51 +0900

    AddAction
    空のカテゴリサーチアクション置き場を設置

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,6 @@
 class CategoriesController < ApplicationController
+
+  def search  # カテゴリサーチ、ツリー表示のカテゴリークリックで返す検索結果
+
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  
   def index # トップページ、アイテムをカテゴリー別に最新投稿順番に
   
   end
