01ac470b2b4006ce11dc40bce935ee9a8cdc6332
yufuworks <yufuworks@outlook.jp>
2019-05-11 00:04:20 +0900

    AddFunctionDigestAuthentication
    ・Basic認証の代わりにDigest認証を実装
    ・ローカルでの動作を確認
    ・マージしてみてサーバーの動作確認をするため、コメントアウトでスイッチできるBasic認証機能記述も含まれています。
    ・変数の呼び出し確認用のダミーviewページを作っています。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
+require 'digest/md5'
 class ApplicationController < ActionController::Base
+  # before_action :basic_auth
+  # protect_from_forgery with: :exception
+
+  # private
+
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     # username == 'admin' && password == '2222'
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+
+  #     # username == Rails.application.credentials.basic_auth[:user] && password == Rails.application.credentials.basic_auth[:password]
+  #   end
+  # end
+
+
+    before_action :authenticate
+    username = Rails.application.credentials.basic_auth[:user]
+    password = Rails.application.credentials.basic_auth[:password]
+  
+    REALM = 'SecretZone'.freeze
+    USERS = { username => Digest::MD5.hexdigest([username, REALM, password].join(':'))}.freeze
+  
+    private
+  
+    def authenticate
+      authenticate_or_request_with_http_digest(REALM) do |username|
+        USERS[username]
+      end
+    end
 
 end
