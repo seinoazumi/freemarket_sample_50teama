5caa583d3af8a0e4bc236f47da47a55cae1d9958
yufuworks <yufuworks@outlook.jp>
2019-05-11 12:33:56 +0900

    BuildFunctionDigestAuthentication
    Digest認証機能の正常動作を確認できたので実装完了
    不要な記述、メソッドの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,9 @@
 require 'digest/md5'
+
 class ApplicationController < ActionController::Base
-  # basic認証機能、カリキュラムコピペ、変数のみcredentialsから取り出すものに変更
-  
-  # before_action :basic_auth
-  # protect_from_forgery with: :exception
-
-  # private
-
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     # username == 'admin' && password == '2222'
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-
-  #     # username == Rails.application.credentials.basic_auth[:user] && password == Rails.application.credentials.basic_auth[:password]
-  #   end
-  # end
-
 
     before_action :authenticate
+    
     username = Rails.application.credentials.basic_auth[:user]
     password = Rails.application.credentials.basic_auth[:password]
   
