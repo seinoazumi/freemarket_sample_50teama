69599cdd87172e755c0a0790e0745983a249bbb4
yufuworks <yufuworks@outlook.jp>
2019-05-11 16:03:46 +0900

    FixingJsCompileError
    jpegをjpgに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
   # TODO: デプロイ時にjsコンパイル起因とみられるrake aborted! Uglifier::Error: Unexpected character '`' が発生
   # 以下の本番環境コンフィグを調整中
   # config.assets.js_compressor = :uglifier
-  # config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.assets.js_compressor = Uglifier.new(harmony: false)
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.js_compressor = Uglifier.new(harmony: false)
   
 
   # config.assets.css_compressor = :sass
