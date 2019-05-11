976363fb83a6bae95fbb3e4e8bb37dfc519480e4
okutamatanuki <seinoazumi@gmail.com>
2019-05-11 11:14:58 +0900

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,7 @@ class UsersController < ApplicationController
   def show  # ユーザー個人ページ、自分の出品した商品を出品ステータス別に得る
 
   end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+  
   root 'items#index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :destroy]
   resources :items, only: [:index, :show, :new, :create] do
     resources :categories, only: [:search]
   end
