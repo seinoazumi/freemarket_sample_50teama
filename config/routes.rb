9761f0395deac4669bc84ddc903b26117a5ed0a3
yufuworks <yufuworks@outlook.jp>
2019-05-12 20:14:09 +0900

    breakpoint
    ざっくり完成。
    残り作業
    ・レンダー埋め込み
    　パンくず
    　l-side
    ・パーシャル埋め込み
    　リスト表示部html+css

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'items#index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
   resources :items, only: [:index, :show, :new, :create] do
     resources :categories, only: [:search]
   end
