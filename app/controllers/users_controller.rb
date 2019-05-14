60dd270c9cb0211a14f858d11f303ed4b7786093
Akinori0123 <akiaki214@icloud.com>
2019-05-14 18:42:00 +0900

    add-update-user-information-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,21 @@ class UsersController < ApplicationController
 
   def logout
   end
-  
 
   def edit
+    @user = User.find(current_user.id)
     render "users/mypage/#{params[:name]}"
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to(root_path)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:postal_code,:prefecture,:city,:address,:building,:phone)
   end
 end
