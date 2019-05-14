dfceae1f9f3f0b1d5a049c7782312d82b2ffa2ab
Akinori0123 <akiaki214@icloud.com>
2019-05-14 16:24:20 +0900

    edit-routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   get 'edit' => 'users#edit'
   # ルーティングは追って検討する
 
-  resources :users, only: [:show, :new]
+  get 'users/mypage/:name', controller: 'users', action: 'edit'
+
+  resources :users, only: [:show, :new, :edit, :update]
   resources :items, only: [:index, :show, :new, :create] do
     resources :categories, only: [:search]
   end
