0872fcc6dc4ed764def3d735855b40674b5733d7
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-05-15 20:09:38 +0900

    Create user_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_043035) do
+ActiveRecord::Schema.define(version: 2019_05_15_110221) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2019_05_12_043035) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "user_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_user_items_on_item_id"
+    t.index ["user_id"], name: "index_user_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,4 +106,6 @@ ActiveRecord::Schema.define(version: 2019_05_12_043035) do
   add_foreign_key "images", "items"
   add_foreign_key "likes", "items"
   add_foreign_key "likes", "users"
+  add_foreign_key "user_items", "items"
+  add_foreign_key "user_items", "users"
 end
