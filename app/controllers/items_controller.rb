04ac4df0940e040af618c38303f73daae425abda
yufuworks <yufuworks@outlook.jp>
2019-05-15 17:37:45 +0900

    breakpoint
    特に進展なし、seedを作るため一時離脱

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,22 @@
 class ItemsController < ApplicationController
+  # before_action :set_current_user
 
   def index # トップページ、アイテムをカテゴリー別に最新投稿順番に
     @items = Item.all.order(id: "DESC")
   end
 
   def new
-    @item = Item.new(params_permit)
   end
 
   def create
+    @item = Item.new(params_permit)
+    if @item.save
+      redirect_to group_messages_path(@group)
+      # モーダル表示させる
+    else
+      render :new
+      # 入力不備部分に赤字表示させる
+    end
   end
 
   def show
@@ -16,6 +24,10 @@ class ItemsController < ApplicationController
 
   private
 
-  def params_permit
-    params.require(:items).permit(:name, :dondition, :delivery_fee, :delivery_days, :price, :buyer_id, :seller_id).merge(images: [])
+  # def params_permit
+  #   params.require(:items).permit(:name, :dondition, :delivery_fee, :delivery_days, :price, :seller_id).merge(images: [])
+
+  # def set_current_user
+  #   @user = User.find(params[current_user.id])
+  # end
 end
