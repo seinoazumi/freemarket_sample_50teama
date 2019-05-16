1f1127fb26c32905f3742318dfb3786881e86863
okutamatanuki <seinoazumi@gmail.com>
2019-05-16 13:46:26 +0900

    Revert commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,6 @@ class UsersController < ApplicationController
 
   def logout
   end
+  
 
-  def edit
-  end
-
-  def update
-    @user = User.find(current_user.id)
-    if @user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:postal_code,:prefecture,:city,:address,:building,:phone)
-  end
 end
