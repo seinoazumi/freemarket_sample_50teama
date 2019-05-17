9b3e07c08cc2be660f9fef1de7b4484fa076c9fe
Akinori0123 <akiaki214@icloud.com>
2019-05-17 16:36:43 +0900

    add-verify_recaptcha-to-signin-and-signup-controller
    ロボット認証ができていない場合、ログインと新規登録ができないように条件分岐

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,7 +10,11 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
+    if verify_recaptcha
      super
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   # DELETE /resource/sign_out
