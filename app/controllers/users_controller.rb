8273bebac980b215e9efd5c086c604e1f0d1f9ab
okutamatanuki <seinoazumi@gmail.com>
2019-05-17 16:30:22 +0900

    Reduce the file changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    render "users/mypage/#{params[:url]}"
   end
 
   def update
@@ -83,7 +84,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name,:email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday,:postal_code,:prefecture,:city,:address,:building,:phone)
+    params.require(:user).permit(:name,:email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday,:postal_code,:prefecture,:city,:address,:building,:phone,:profile)
   end
 
 end
