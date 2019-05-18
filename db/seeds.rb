94ddd1fee64b81206aaa7cc4c6a8269f80e49876
yufuworks <yufuworks@outlook.jp>
2019-05-18 14:56:46 +0900

    FixedValues
    検証しやすくするため、各項目の値をuserID, itemIDと連動させて変化をもたせるように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 20.times do |num|
-  User.create(name: "user#{num + 1}", email: "user#{num + 1}@gmail.com", password: "11111111", first_name: "寧夢#{num}", last_name: "遊座#{num}", first_name_kana: "ネイム", last_name_kana: "ユウザ", postal_code: 1000000 + num, prefecture: num + 1, city:"保外市",building: "富嶽ビル", address:"#{num}", phone: 9000000000 + num + num * 10000, birthday: '2000-01-01', money: 50000, point: 3000, seller_id: num + 1, buyer_id: num + 1, profile: "よろしく！")
+  User.create(name: "user#{num + 1}", email: "user#{num + 1}@gmail.com", password: "11111111", first_name: "寧夢#{num + 1}", last_name: "遊座#{num + 1}", first_name_kana: "ネイム", last_name_kana: "ユウザ", postal_code: 1000000 + num, prefecture: num % 47 + 1, city:"保外市#{num + 1}",building: "富嶽ビル#{num + 1}", address:"#{num + 1}", phone: 9000010001 + num + num * 10000, birthday: '2000-01-01', money: 50000 + (num + 1) * 1000, point: 3000 + (num + 1) * 100, seller_id: num + 1, buyer_id: num + 1, profile: "user#{num + 1}です、よろしく！")
 end
 
 60.times do |num|
-  Item.create(name: "お買い得セット#{num + 1}", condition: 0, delivery_fee: 1000, delivery_days: 0, price: (num + 1) * 1000)
+  Item.create(name: "お買い得セット#{num + 1}", condition: num % 6 + 1, delivery_fee: 1000 + (num + 1) * 10, delivery_days: num % 4 + 1, price: (num + 1) * 1000)
 end
 
 60.times do |num|
