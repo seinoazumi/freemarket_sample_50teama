cf4e256c1ac1e2c26af0f7c8f0ea55e2e94cd8b4
okutamatanuki <seinoazumi@gmail.com>
2019-05-18 20:43:53 +0900

    Modifies smoothing

--- app/assets/javascripts/drop-down-list.js
+++ app/assets/javascripts/drop-down-list.js
@@ -2,7 +2,8 @@ $(document).on('turbolinks:load', function() {
 
   $('#notification-list__root').on("mouseover", function(){
     $('#notification-list').css('display','block');
-    });
+      $('#todo-list').css('display','none');
+    }); // end #notification-list の dysplayがblockの時の処理
   }); //「お知らせ」ボタンに触るとリストが表示される
 
   $(document).on('turbolinks:load', function() {
@@ -14,18 +15,20 @@ $(document).on('turbolinks:load', function() {
       $(this).css('display','none');
   }); //リストからマウスを離すと、0.5秒後にリストは見えなくなる
 
+  //---------------------------------------------やることリストのJS
   $('#todo-list__root').on("mouseover", function(){
     $('#todo-list').css('display','block');
-    });
-  }); //「お知らせ」ボタンに触るとリストが表示される
+      $('#notification-list').css('display','none');
+    }); // end #todo-list の dysplayがblockの時の処理
+  }); // end mouse over 時の処理
 
   $(document).on('turbolinks:load', function() {
     $('#todo-list').on("mouseover", function(){
       $(this).css('display','block');
-      }); //表示されたリストを触り続けている間はリストが表示され続ける
+      });
 
     $('#todo-list').delay(500).on("mouseout", function(){
       $(this).css('display','none');
-  }); //リストからマウスを離すと、0.5秒後にリストは見えなくなる
+  });
 
 });
