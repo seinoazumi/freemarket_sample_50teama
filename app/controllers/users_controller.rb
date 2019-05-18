bba33d0b18c013552ea55120a7dd80b7d3890eaa
okutamatanuki <seinoazumi@gmail.com>
2019-05-18 17:09:34 +0900

    Delete some lows after team reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,22 +86,3 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name,:email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday,:postal_code,:prefecture,:city,:address,:building,:phone,:profile)
   end
-
-  def edit
-  end
-
-  def update
-    @user = User.find(current_user.id)
-    if @user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:postal_code,:prefecture,:city,:address,:building,:phone)
-  end
-end
