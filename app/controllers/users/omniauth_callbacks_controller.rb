4aa444ffd8d79a11e2fb7ede8a3c5dcb6fc2366a
Akinori0123 <akiaki214@icloud.com>
2019-05-19 15:59:41 +0900

    correct-omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     callback_for(:facebook)
   end
+
   def google_oauth2
     callback_for(:google)
   end
+
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
