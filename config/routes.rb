64fb85acc4ff6862edfe3dec9a371b987a0deb23
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-05-20 15:36:40 +0900

    routesの編集(items/show,confirm)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     :omniauth_callbacks => 'users/omniauth_callbacks'
   }
 
-
   root 'items#index'
   get 'edit' => 'users#edit'
   # ルーティングは追って検討する
@@ -20,7 +19,7 @@ Rails.application.routes.draw do
 
   resources :items, only: [:index, :show, :new, :create] do
     collection do
-      get 'trade/:url', action:'show'
+      get ':id/:url', action:'show'
     end
 
     resources :categories, only: [:search]
