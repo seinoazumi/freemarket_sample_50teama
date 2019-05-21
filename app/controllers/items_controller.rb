411bd2b0e408b0e7f27cb82b68e279403a0ab01a
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-05-21 17:08:04 +0900

    購入までのバックエンド
    商品詳細ページ、購入確認ページのデータ取り出し、購入時にカード引き落とし、購入時にbuyer_idをitemに追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  require 'payjp'
 
   def index # トップページ、アイテムをカテゴリー別に最新投稿順番に
     @items = Item.all.order(id: "DESC").limit(4)
@@ -11,9 +12,33 @@ class ItemsController < ApplicationController
   end
 
   def show
+    # カード情報引き出し
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
+    customer = Payjp::Customer.retrieve(current_user.payjp_id)
+    default_card = customer.default_card
+    @card = customer.cards.retrieve(default_card)
+
     @items = Item.all.order(id: 'DESC').limit(4)
     @item = Item.find(params[:id])
     render "/items/#{params[:url]}" if params[:url]
   end
+
+  def pay #カード支払い Itemにbuyer_idを追加
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
+    customer = Payjp::Customer.retrieve(current_user.payjp_id)
+    default_card = customer.default_card
+    card = customer.cards.retrieve(default_card)
+    @item = Item.find(params[:id])
+    charge = Payjp::Charge.create(
+      amount: @item.price,
+      customer: customer,
+      currency: 'jpy',
+    )
+    item= Item.find(params[:id]) 
+    item.update(buyer_id: current_user.id)
+    flash[:notice] = "商品を購入しました"
+    redirect_to root_path
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
         render "/users/signup/address"
       end
     when "complete"
-      Payjp.api_key = 'sk_test_b486b3703ec1656553c4921a'
+      Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
       customer = Payjp::Customer.create(card: params[:payjp_token])
       @user = User.new(name: session[:name],email: session[:email],password: session[:password],password_confirmation: session[:password_confirmation],last_name: session[:last_name],first_name: session[:first_name],last_name_kana: session[:last_name_kana],first_name_kana: session[:first_name_kana],"birthday(1i)": session[:"birthday(1i)"],"birthday(2i)": session[:"birthday(2i)"],"birthday(3i)": session[:"birthday(3i)"],postal_code: session[:postal_code],prefecture: session[:prefecture],city: session[:city],address: session[:address],building: session[:building],phone: session[:phone],payjp_id: customer.id)
       @user[:seller_id] = 1
