9b95730a79c96b8cd258713cea9b52999387d997
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-05-21 20:26:58 +0900

    エラーハンドリングの記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   require 'payjp'
-  before_action :set_item, only: [:show, :destroy]
+  before_action :set_item, only: [:show, :destroy, :pay]
 
   def index # トップページ、アイテムをカテゴリー別に最新投稿順番に
     @items = Item.all.order(id: "DESC").limit(4)
@@ -14,35 +14,37 @@ class ItemsController < ApplicationController
 
   def show
     # カード情報引き出し
-    Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
-    customer = Payjp::Customer.retrieve(current_user.payjp_id)
-    default_card = customer.default_card
-    @card = customer.cards.retrieve(default_card)
-
-    @items = Item.all.order(id: 'DESC').limit(4)
-    @item = Item.find(params[:id])
-    render "/items/#{params[:url]}" if params[:url]
+    if params[:url] == "confirm"
+      if user_signed_in?
+        Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
+        customer = Payjp::Customer.retrieve(current_user.payjp_id)
+        default_card = customer.default_card
+        @card = customer.cards.retrieve(default_card)
+        render "/items/#{params[:url]}"
+      else
+        redirect_to new_user_session_path
+      end
+    else
+      @items = Item.all.order(id: 'DESC').limit(4)
+    end
   end
 
   def pay #カード支払い Itemにbuyer_idを追加
-    Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
-    customer = Payjp::Customer.retrieve(current_user.payjp_id)
-    default_card = customer.default_card
-    card = customer.cards.retrieve(default_card)
-    @item = Item.find(params[:id]) 
-    charge = Payjp::Charge.create(
-      amount: @item.price,
-      customer: customer,
-      currency: 'jpy',
-    )
-    if 
-      @item.update(buyer_id: current_user.id)
-      flash[:notice] = "商品を購入しました"
+      Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
+      customer = Payjp::Customer.retrieve(current_user.payjp_id)
+      default_card = customer.default_card
+      card = customer.cards.retrieve(default_card)
+      charge = Payjp::Charge.create(
+        amount: @item.price,
+        customer: customer,
+        currency: 'jpy',
+      )
+    rescue => e #エラーハンドリング
+      arert
       redirect_to root_path
-    else #決済できなかったら
-      
     end
-
+      @item.update(buyer_id: current_user.id)
+      redirect_to root_path
   end
   
   def edit
@@ -54,16 +56,6 @@ class ItemsController < ApplicationController
     else
       redirect_to item_path(@item)
     end
-  end
-
-  def show
-    @items = Item.all.order(id: "DESC").limit(4)
-    require 'payjp'
-    Payjp.api_key = Rails.application.credentials.payjp[:secret_access_key]
-    customer = Payjp::Customer.retrieve(current_user.payjp_id)
-    @card_id = customer.default_card
-    @card = customer.cards.retrieve(@card_id)
-    render "/items/#{params[:url]}" if params[:url]
   end
 
   def set_item
