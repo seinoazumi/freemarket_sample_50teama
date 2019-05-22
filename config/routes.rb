ef1bc43862e9532e142a527ae0b2516ec5ee7b40
okutamatanuki <seinoazumi@gmail.com>
2019-05-22 20:42:58 +0900

    Modified search bar

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show, :new, :create, :edit, :destroy] do
     collection do
       get ':id/:url', action:'show'
-      get 'search'
+      get 'search', action:'search'
     end
 
     resources :categories, only: [:search]
