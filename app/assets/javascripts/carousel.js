11a21580a77167b585cc70d4adf31089de753e20
okutamatanuki <seinoazumi@gmail.com>
2019-05-23 16:53:37 +0900

    Build slider

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,6 +1,27 @@
+//----------------------------------------top page
 $(document).on('turbolinks:load', function(){
   $('.slick').slick({
     autoplay:true,
     dots:true,
   });
+
+
+//----------------------------------------商品詳細ページ
+
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.thumb'
+  });
+  $('.thumb').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
 });
