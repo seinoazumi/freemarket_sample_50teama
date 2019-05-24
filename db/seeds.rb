2334c1fc979844d835e3b0d5ffd807d81f4ada98
Akinori0123 <akiaki214@icloud.com>
2019-05-24 17:32:38 +0900

    add-category_id-to-items-table

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,7 +71,8 @@ end
     detail: "この文章は商品説明用です！この商品のIDは#{num}です！yay!",
     delivery_prefecture: rand(1..48),
     delivery_method: rand(1..9),
-    status: rand(0..2)
+    status: rand(0..2),
+    category_id: rand(0..10)
     )
 end
 
