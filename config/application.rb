79c33d8332af21b05a681448777daa075bf09e77
yufuworks <yufuworks@outlook.jp>
2019-05-25 15:59:33 +0900

    AddConfigurations
    enum_help実装の設定を変更
    モデルの指定

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,14 @@ module FreemarketSample50teama
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :ja
+    # config.i18n.available_locales = [:ja, :en]
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
-    config.i18n.available_locales = [:ja, :en]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
