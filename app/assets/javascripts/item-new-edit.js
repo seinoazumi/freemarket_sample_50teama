3af9150a30f0a03ba312dca095ab8a4c7ad22eaa
okutamatanuki <seinoazumi@gmail.com>
2019-05-27 08:14:32 +0900

    Modify view

--- app/assets/javascripts/item-new-edit.js
+++ app/assets/javascripts/item-new-edit.js
@@ -1,11 +1,11 @@
 $(document).on('turbolinks:load', function() {
   // もし、画像の数が５個以上か10個なら、ドラッグ&ドロップエリアを消す
   var size = $(".upload-item").length;
-  if (size >= 5){
-    $("#drop_zone1").remove();
+  if (size == 4){
+    $("#drop_zone").css("width","18%");
   }
   if (size == 10){
-    $("#drop_zone2").remove();
+    $("#drop_zone").remove();
   }
   // 「削除」ボタンがクリックされ、見えないチェックボックスにチェックが入った時に、アイテムを削除する（見た目上）
   // データベースには、「送信」ボタンを押した時に反映される
