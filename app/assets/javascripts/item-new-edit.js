2d2ec9561247d810c3f806a635e9f5d1268afcba
okutamatanuki <seinoazumi@gmail.com>
2019-05-27 05:50:46 +0900

    Add comments

--- app/assets/javascripts/item-new-edit.js
+++ app/assets/javascripts/item-new-edit.js
@@ -7,6 +7,8 @@ $(document).on('turbolinks:load', function() {
   if (size == 10){
     $("#drop_zone2").remove();
   }
+  // 「削除」ボタンがクリックされ、見えないチェックボックスにチェックが入った時に、アイテムを削除する（見た目上）
+  // データベースには、「送信」ボタンを押した時に反映される
   $(".delete-check-box").on("click", function(){
     if($(this).prop('checked') == true){
       $(this).parents(".upload-item").remove();
