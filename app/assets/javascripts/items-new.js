45d34750b1ca1df705aefbe376c5bb6374adb947
Akinori0123 <akiaki214@icloud.com>
2019-05-27 17:34:41 +0900

    add-javascript-to-items-new
    価格を入力すると、販売手数料と販売利益が自動で表示されるようにする

---
+++ app/assets/javascripts/items-new.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function() {
+  $('.input-part__input').on("keyup", function(){
+    if($(this).val() >= 300 && $(this).val() <= 9999999) {
+      var fee_output = Math.floor($(this).val() * 0.1);
+      $('.input-part__price--fee--output').text(fee_output);
+      $('.input-part__price--total--output').text($(this).val() - fee_output);
+
+    }else {
+      $('.input-part__price--fee--output').text('-');
+      $('.input-part__price--total--output').text('-');
+    }
+  });
+});
