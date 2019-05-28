242849ecc7475ee2d5fb7e3fd0492da95c7ac66c
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-05-28 12:01:16 +0900

    enum化

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,6 +19,18 @@ class Item < ApplicationRecord
     validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 
+  enum condition: {
+    new_one: 1, almost_new: 2, no_scratchies: 3, little_worn: 4, worn: 5, bad: 6
+  }
+
+  enum delivery_cost: {
+    exhibitr_barden: 1, buyer_barden: 2
+  }
+
+  enum delivery_day: {
+    a_day: 1, a_couple_of_days: 2, a_week: 3
+  }
+
   enum delivery_prefecture: {
     hokkaido: 1, aomori: 2, iwate: 3, miyagi: 4, akita: 5, yamagata: 6, fukushima: 7,
     ibaraki: 8, tochigi: 9, gunma: 10, saitama: 11, chiba: 12, tokyo: 13, kanagawa: 14,
@@ -31,19 +43,12 @@ class Item < ApplicationRecord
     undicided: 48
   }, _prefix: true
 
-  enum condition: {
-    new_one: 1, almost_new: 2, no_scratchies: 3, little_worn: 4, worn: 5, bad: 6
-  }
-
-  enum delivery_cost: {
-    exhibitr_barden: 1, buyer_barden: 2
-  }
-
   enum delivery_method: {
     undicided: 1, mercari: 2, u_mail: 3, letter_pack: 4, usealy: 5, yamato: 6, u_pack: 7, click_post: 8, u_packet: 9
   }, _prefix: true
 
-  enum delivery_day: {
-    a_day: 1, a_couple_of_days: 2, a_week: 3
-  }
+  enum status: {
+    not_sale: 0, sale:1, soldout:2
+  }, _prefix: true
+
 end
