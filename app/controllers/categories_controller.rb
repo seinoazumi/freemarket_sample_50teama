3e92da47b2fe6c04d34ac60c4b1b04380d9e40a8
Akinori0123 <akiaki214@icloud.com>
2019-05-29 15:17:22 +0900

    edit-category-show-view
    レディース商品一覧画面に、レディースの子カテゴリの商品と、レディースの孫カテゴリの商品を追加
    他のカテゴリに関しても同様

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,19 @@
 class CategoriesController < ApplicationController
   before_action :set_categories, only: :show
   before_action :set_category, only: :show
+  before_action :set_items, only: :show
 
   def show
-    @items = Item.where(category_id: @category.id)
   end
 
   private
 
   def set_category
     @category = Category.find(params[:id])
+  end
+
+  def set_items
+    category_ids = Category.find(params[:id]).descendant_ids.push(Category.find(params[:id]).id)
+    @items = Item.where(category_id: category_ids, status: 1).order(id: "DESC")
   end
 end
