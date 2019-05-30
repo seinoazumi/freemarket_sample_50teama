6a439380f8ec1e6c0bd2deaed701832c1d7170e9
okutamatanuki <seinoazumi@gmail.com>
2019-05-30 15:21:26 +0900

    commit

--- app/assets/javascripts/item-box.js
+++ app/assets/javascripts/item-box.js
@@ -1,4 +1,11 @@
 $(document).on('turbolinks:load', function() {
   var imgHeight = $(".item-box__photo").width();
   $(".item-box__photo").height(imgHeight);
+
+  $('.item-box__info__name').each(function(){
+    if($(this).text().length > 22){
+      var trim = $(this).text().substr(0, 21) + '...';
+      $(this).text(trim);
+    };
+  });
 });
